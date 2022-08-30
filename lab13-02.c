649f142ce7934cb9be9f7c9e415361ff40ab78ff
Adrian de Freitas <Adefreit@users.noreply.github.com>
2022-08-30 15:26:34 -0600

    Uploading Template

---
+++ lab13-02.c
@@ -0,0 +1,17 @@
+/** lab13-02.c
+ * =========================================================== 
+ * Name: name, date
+ * Section: your section
+ * Project: assignment information 
+ * Purpose: high level description of purpose of the program
+ *          could be multiple lines
+ * =========================================================== */
+
+#include <stdio.h>
+#include "lab13functs.h"
+
+int main() {
+    
+    return 0;
+
+}
